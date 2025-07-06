216d6fba6c1561233782f56d724a5ebb4b60ebd0
cppHusky <cpphusky@outlook.com>
2025-07-06 22:21:58 +0800

    added finished tasks in Jul 06, 2026

---
+++ playground/medium/00612-medium-kebabcase.ts
@@ -0,0 +1,60 @@
+/*
+  612 - KebabCase
+  -------
+  by Johnson Chu (@johnsoncodehk) #medium #template-literal
+
+  ### Question
+
+  Replace the `camelCase` or `PascalCase` string with `kebab-case`.
+
+  `FooBarBaz` -> `foo-bar-baz`
+
+  For example
+
+  ```ts
+  type FooBarBaz = KebabCase<"FooBarBaz">
+  const foobarbaz: FooBarBaz = "foo-bar-baz"
+
+  type DoNothing = KebabCase<"do-nothing">
+  const doNothing: DoNothing = "do-nothing"
+  ```
+
+  > View on GitHub: https://tsch.js.org/612
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type MyMap={'A':'a','B':'b','C':'c','D':'d','E':'e','F':'f','G':'g','H':'h','I':'i','J':'j','K':'k','L':'l','M':'m','N':'n','O':'o','P':'p','Q':'q','R':'r','S':'s','T':'t','U':'u','V':'v','W':'w','X':'x','Y':'y','Z':'z'}
+type Head<S extends string>=S extends keyof MyMap
+	?MyMap[S]
+	:S
+type Tail<S extends string>=S extends `${infer A}${infer R}`
+	?A extends keyof MyMap
+		?`-${MyMap[A]}${Tail<R>}`
+		:`${A}${Tail<R>}`
+	:S
+type KebabCase<S extends string> = S extends `${infer A}${infer R}`
+	?`${Head<A>}${Tail<R>}`
+	:S
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<KebabCase<'FooBarBaz'>, 'foo-bar-baz'>>,
+  Expect<Equal<KebabCase<'fooBarBaz'>, 'foo-bar-baz'>>,
+  Expect<Equal<KebabCase<'foo-bar'>, 'foo-bar'>>,
+  Expect<Equal<KebabCase<'foo_bar'>, 'foo_bar'>>,
+  Expect<Equal<KebabCase<'Foo-Bar'>, 'foo--bar'>>,
+  Expect<Equal<KebabCase<'ABC'>, 'a-b-c'>>,
+  Expect<Equal<KebabCase<'-'>, '-'>>,
+  Expect<Equal<KebabCase<''>, ''>>,
+  Expect<Equal<KebabCase<'😎'>, '😎'>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/612/answer
+  > View solutions: https://tsch.js.org/612/solutions
+  > More Challenges: https://tsch.js.org
+*/
